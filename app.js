24e5eb24e809d791fc3017f4f7c882c2d52b0506
Reinaldo <reinaldopared@gmail.com>
2024-02-13 15:30:06 -0400

    Update

--- app.js
+++ app.js
@@ -169,7 +169,22 @@ function attack() {
 	text.innerText +=
 		" You attack it with your " + weapons[currentWeapon].name + ".";
 	health -= monsters[fighting].level;
-	monsterHealth -= weapons[currentWeapon].power;
+	healthText.innerText = health;
+	monsterHealth -=
+		weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1;
+	monsterHealthText.innerText = monsterHealth;
+	if (health <= 0) {
+		lose();
+	} else if (monsterHealth <= 0) {
+		defeatMonster();
+	}
 }
 
-function dodge() {}
+function dodge() {
+	text.innerText =
+		"You dodge the attack from the " + monsters[fighting].name + ".";
+}
+
+function defeatMonster() {}
+
+function lose() {}
